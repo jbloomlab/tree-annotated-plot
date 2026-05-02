bd1e5174fb15d352d0c5183fd1438533850c9bc5
jbloom <jbloom@fredhutch.org>
2026-05-02 06:32:14 -0700

    single source version number

--- src/tree_annotated_plot/__init__.py
+++ src/tree_annotated_plot/__init__.py
@@ -1,8 +1,10 @@
 """tree-annotated-plot: plots with a categorical axis aligned to a phylogenetic tree."""
 
+from importlib.metadata import version
+
 from ._plot import plot
 from ._tree import TreeNode, load_auspice
 
-__version__ = "0.0.1"
+__version__ = version("tree-annotated-plot")
 
 __all__ = ["plot", "load_auspice", "TreeNode", "__version__"]
